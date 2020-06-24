6325d506e5f363dafe02c6ae6783cdd0a4dbdd22
jeyroik <root@ovz6.funcraft.px7zm.vps.myjino.ru>
2020-06-24 10:54:26 +0000

    init

---
+++ tests/getters/GettersTest.php
@@ -0,0 +1,15 @@
+<?php
+namespace tests\getters;
+
+use extas\components\packages\PackageInstallCase;
+
+/**
+ * Class GettersTest
+ *
+ * getters tests/getters
+ * @author jeyroik
+ */
+class GettersTest extends PackageInstallCase
+{
+
+}
